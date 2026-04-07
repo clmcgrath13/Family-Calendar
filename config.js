34dd36329bc7b986c66d67a8cfbc05154ce90109
clmcgrath13 <clmcgrath13@gmail.com>
2026-04-07 15:08:17 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 window.GCAL_CLIENT_ID = '';
 window.GCAL_API_KEY = 'AIzaSyAPN3xD3OcNnkRa0pPm7PS7aHvPydPzMDg';
 window.GCAL_CALENDAR_ID = 'clmcgrath13@gmail.com'; // or leave out for primary
+window.GITHUB_TOKEN = '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';
