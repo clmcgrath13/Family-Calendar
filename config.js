77745fc75dd0201543721fd5c65160391debe59e
clmcgrath13 <clmcgrath13@gmail.com>
2026-04-08 07:56:45 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GCAL_CLIENT_ID = '';
 window.GCAL_API_KEY = 'AIzaSyAPN3xD3OcNnkRa0pPm7PS7aHvPydPzMDg';
 window.GCAL_CALENDAR_ID = 'clmcgrath13@gmail.com'; // or leave out for primary
-window.GITHUB_TOKEN = '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';
+window.GITHUB_TOKEN = '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';
 window.GITHUB_OWNER = 'clmcgrath13';
 window.GITHUB_REPO  = 'Family-Calendar';
