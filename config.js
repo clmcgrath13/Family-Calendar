8762a1192a8d25bdd9d34613a422e9a5076c4843
clmcgrath13 <clmcgrath13@gmail.com>
2026-04-08 08:34:09 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GCAL_CLIENT_ID = '';
 window.GCAL_API_KEY = 'AIzaSyAPN3xD3OcNnkRa0pPm7PS7aHvPydPzMDg';
 window.GCAL_CALENDAR_ID = 'clmcgrath13@gmail.com'; // or leave out for primary
-window.GITHUB_TOKEN = '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';
+window.GITHUB_TOKEN = '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';
 window.GITHUB_OWNER = 'clmcgrath13';
 window.GITHUB_REPO  = 'Family-Calendar';
