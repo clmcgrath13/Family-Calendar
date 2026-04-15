0e41e69f231910c01761f6b3e40cc2025574bbd9
clmcgrath13 <clmcgrath13@gmail.com>
2026-04-15 10:08:13 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.GCAL_CLIENT_ID = '';
+//oauth 2.0 client ID 697995087846-jb5286il5gl9c0osnvgbtjheu3u844ne.apps.googleusercontent.com
 window.GCAL_API_KEY = 'AIzaSyAPN3xD3OcNnkRa0pPm7PS7aHvPydPzMDg';
 window.GCAL_CALENDAR_ID = 'clmcgrath13@gmail.com'; // or leave out for primary
 window.GITHUB_TOKEN = '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';
